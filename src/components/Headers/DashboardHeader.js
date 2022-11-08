d50afef47f481a2d4b50607f19d79714db9c83a0
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2022-11-07 22:14:25 -0800

    supertokens implement

--- src/components/Headers/DashboardHeader.js
+++ src/components/Headers/DashboardHeader.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { signOut } from "supertokens-auth-react/recipe/passwordless";
 
 export default function DashboardHeader() {
+  async function onLogout() {
+    await signOut();
+    window.location.href = "/";
+  }
   return (
     <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
       <a className="navbar-brand col-sm-3 col-md-2 mr-0 px-3" href="/">Cathy Loerzel</a>
       <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
       <ul className="navbar-nav px-3">
         <li className="nav-item text-nowrap">
-          <a className="nav-link" href="/">Sign Out</a>
+          <div className="nav-link" onClick={onLogout}>Sign Out</div>
         </li>
       </ul>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,40 +1,71 @@
+// React Dependencies
 import React from 'react';
-import { render } from "react-dom"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import App from "./App"
+import { render } from "react-dom";
+import * as reactRouterDom from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// SuperTokens
+import SuperTokens, { SuperTokensWrapper, getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
+import Passwordless from "supertokens-auth-react/recipe/passwordless";
+import Session, { SessionAuth } from "supertokens-auth-react/recipe/session";
 
 // layout
-
+import App from "./App"
 import Dashboard from "./layouts/Dashboard"
 import Event from "./layouts/Event"
 import Blog from "./layouts/Blog"
 
 // components
-
 import BlogForm from "./components/Forms/BlogForm"
 import BlogsGrid from "./components/Grids/BlogsGrid"
 import EventForm from "./components/Forms/EventForm"
 import EventsGrid from "./components/Grids/EventsGrid"
 
 const rootElement = document.getElementById("root")
+
+// SuperTokens
+SuperTokens.init({
+  appInfo: {
+      appName: "Cathy Admin Dashboard",
+      apiDomain: process.env.REACT_APP_API_BASE_URL,
+      websiteDomain: "http://localhost:3000",
+      apiBasePath: "/auth",
+      websiteBasePath: "/login"
+  },
+  recipeList: [
+      Passwordless.init({
+          contactMethod: "EMAIL"
+      }),
+      Session.init()
+  ]
+});
+
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Dashboard />} />
-        <Route path="blogs">
-          <Route path="new" element={<BlogForm />} />
-          <Route path=":blogId" element={<Blog />} />
-          <Route index element={<BlogsGrid />} />
-        </Route>
-        <Route path="events">
-          <Route path="new" element={<EventForm />} />
-          <Route path=":eventId" element={<Event />} />
-          <Route index element={<EventsGrid />} />
+  <SuperTokensWrapper>
+    <BrowserRouter>
+      <Routes>
+        {/*This renders the login UI on the /login route*/}
+        {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+        <Route path="/" element={
+          <SessionAuth>
+            <App />
+          </SessionAuth>
+        }>
+          <Route index element={<Dashboard />} />
+          <Route path="blogs">
+            <Route path="new" element={<BlogForm />} />
+            <Route path=":blogId" element={<Blog />} />
+            <Route index element={<BlogsGrid />} />
+          </Route>
+          <Route path="events">
+            <Route path="new" element={<EventForm />} />
+            <Route path=":eventId" element={<Event />} />
+            <Route index element={<EventsGrid />} />
+          </Route>
         </Route>
         <Route path="*" element={<h1>NOT FOUND</h1>} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+      </Routes>
+    </BrowserRouter>
+  </SuperTokensWrapper>,
   rootElement
 )
