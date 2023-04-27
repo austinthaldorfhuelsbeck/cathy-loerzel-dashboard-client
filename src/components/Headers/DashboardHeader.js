e352876d1f2b99199ab3b55e0dbea7bbab0ea638
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2023-04-27 16:25:18 -0700

    remove signout

--- src/components/Headers/DashboardHeader.js
+++ src/components/Headers/DashboardHeader.js
@@ -4,9 +4,9 @@ export default function DashboardHeader() {
       <a className="navbar-brand col-sm-3 col-md-2 mr-0 px-3" href="/">Cathy Loerzel</a>
       <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
       <ul className="navbar-nav px-3">
-        <li className="nav-item text-nowrap">
+        {/* <li className="nav-item text-nowrap">
           <div className="nav-link">Sign Out</div>
-        </li>
+        </li> */}
       </ul>
     </nav>
   )
